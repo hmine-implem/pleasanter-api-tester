26a7927ea713dceff926c10f670f9f0607cad9db
hmine-implem <141791140+hmine-implem@users.noreply.github.com>
2025-04-16 17:38:38 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+public class Program
+{
+    private const string ApiUrl = "";
+    private const string ApiKey = "";
+    private static readonly HttpClient httpClient = new(){
+        BaseAddress = new Uri(ApiUrl),
+        Timeout = TimeSpan.FromSeconds(3000)
+    };
+
+    // ■本ツールで実際に行ったテストについて
+    // Test1. bulkdelete [使い方] https://pleasanter.org/ja/manual/api-table-bulk-delete
+    private static readonly List<string> TEST_1_1_SELECTED =
+    [
+        "9998",
+        "9999"
+    ];
+
+    static async Task Main()
+    {
+        try {
+            await PostAsync(httpClient);
+        } catch (Exception ex) {
+            Console.WriteLine(ex.Message);
+        } finally {
+            httpClient.Dispose();
+            Console.WriteLine("Complete.");
+        }
+    }
+    
+    // ■WHAT'S TEST?
+    // Test1-1. Selected = TEST_1_1_SELECTED
+
+    static async Task PostAsync(HttpClient httpClient)
+    {
+        using StringContent jsonContent = new(
+            content: JsonConvert.SerializeObject(new PleasanterApiFieldes {
+                ApiKey = ApiKey,
+                ApiVersion = "1.0",
+                {WHAT'S TEST?} }),
+            encoding: Encoding.UTF8,
+            mediaType: "application/json");
+        using HttpResponseMessage response = await httpClient.PostAsync(
+            requestUri: httpClient.BaseAddress,
+            content: jsonContent);
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        Console.WriteLine($"{jsonResponse}\n");
+    }
+}
+
+public class PleasanterApiFieldes
+{
+    [JsonProperty("ApiVersion")]
+    public required string ApiVersion { get; set; }
+    [JsonProperty("ApiKey")]
+    public required string ApiKey { get; set; }
+    [JsonProperty("Selected")]
+    public List<string>? Selected { get; set; }
+}
